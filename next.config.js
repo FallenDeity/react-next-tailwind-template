1569c00295521f91883addb1d27b02e120595ca9
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2024-10-02 04:16:05 +0530

    feat: Enable turbo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	experimental: {
+		turbo: {
+			resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
+		},
+	},
 	reactStrictMode: true,
 	distDir: "dist",
 }
